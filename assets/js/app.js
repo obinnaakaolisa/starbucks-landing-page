0b0f443115f10cf5fa968c0f74e5a23fd74fdbd5
obinnaakaolisa <akaolisaobinna@gmail.com>
2020-11-18 14:25:20 +0100

    Finished Design Coding

---
+++ assets/js/app.js
@@ -0,0 +1,9 @@
+function imgSlider(imgSrc) {
+    const thumb = document.querySelector('.starbucks').src = imgSrc;
+}
+
+function changeCircleColor(color) {
+    const circle = document.querySelector('.circle');
+    circle.style.background = color;
+    circle.style.transition = '.5s';
+}
